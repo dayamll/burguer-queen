ab9bab1d4364bfff44939743581bc90d4ed43f0b
Danielapariona <daniela.parionac@gmail.com>
2018-03-08 11:03:38 -0500

    Show hello world in development environment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hola mundo')
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const app = document.getElementById('app');
+const holaMundo = <h1>hola mundo!</h1>;
+
+ReactDom.render(holaMundo, app);
